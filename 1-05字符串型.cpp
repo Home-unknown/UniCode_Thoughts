43d1f120e23b2b4231836ee276876a14bca59c65
Home-unknown <Home_unknown@163.com>
2026-05-16 17:24:33 +0800

    Add files via upload

---
+++ 1-05字符串型.cpp
@@ -0,0 +1,18 @@
+/*
+#include<iostream>
+using namespace std;
+#include<string>			//用C++风格字符串时候，要包含这个头文件
+
+int main()
+{
+	char str[] = "hello world";	//C风格字符串	注意事项:1.char 字符串名 [] 
+	cout << str << endl;		//						 2.等号后面要用双引号包含起来字符串
+	
+	string str2 = "hello world";//C++风格字符串  注意事项:包含一个字符串
+	cout << str2 << endl;
+
+	system("pause");
+
+	return 0;
+}
+*/
