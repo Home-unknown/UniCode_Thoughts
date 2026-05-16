57c59c7944c6bbb01b326140f7cc0ae963724a9b
Home-unknown <Home_unknown@163.com>
2026-05-16 17:24:59 +0800

    Add files via upload

---
+++ 1-06布尔类型bool.cpp
@@ -0,0 +1,18 @@
+/*
+//布尔数据类型代表真或假的值
+#include<iostream>		
+using namespace std;
+
+int main()
+{
+	bool flag1 = true;		//true代表真
+	cout << flag1 << endl;
+
+	bool flag2 = false;		//false代表假
+	cout << flag2 << endl;	//本质上：真true代表1，假false代表假
+
+	system("pause");
+
+	return 0;
+}
+*/
