493245a9beeebb1c8bd9f77a6be7ad4ae8b6cf59
Home-unknown <Home_unknown@163.com>
2026-05-16 17:22:54 +0800

    Add files via upload

---
+++ 1-02整型.cpp
@@ -0,0 +1,15 @@
+/*
+//整型 short(2) int(4) long(4) long long(8)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num = 10;
+	cout << "占用\n" << sizeof(num)<<endl;
+	system("pause");
+	
+	return 0;
+}
+*/
