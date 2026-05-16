5b0f74e44b87c1630e98f05294dc9d5e3019eb54
Home-unknown <Home_unknown@163.com>
2026-05-16 17:24:06 +0800

    Add files via upload

---
+++ 1-04字符型.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char ch1= 'a';		//单引号只能用一个字符,不能使用字符串
+	
+	cout << ch1 << endl;
+
+	sizeof(char);       //占用一个字节
+ 
+	//char ch2="b";		  不能用双引号
+ 
+	//char ch3='abc';	  单引号内只能有一个字符			
+
+	cout << int(ch1) << endl;		//将a用ASCII码转为10进制数字
+
+	//a-97 A-65 space-32
+
+	system("pause");
+
+	return 0;
+}
+*/
